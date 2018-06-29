1de16550f46752231c426e450f459ee8d09ccd17
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2018-06-30 04:57:48 +0800

    commit

--- MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/RewardsActivity.java
+++ MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/RewardsActivity.java
@@ -1,8 +1,11 @@
 package ph.edu.dlsu.takoyaki.myapplication;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RewardsActivity extends AppCompatActivity {
 
@@ -10,6 +13,20 @@ public class RewardsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewards);
+
+        Button myrewards = (Button) findViewById(R.id.rewards_myrewards);
+
+        myrewards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+
+                i.setClass(getBaseContext(), MyRewardsActivity.class);
+                startActivity(i);
+
+
+            }
+        });
 
         TabLayout rewardsTabLayout = (TabLayout) findViewById(R.id.rewards_tabLayout);
         rewardsTabLayout.addTab(rewardsTabLayout.newTab().setText("Gold"));
