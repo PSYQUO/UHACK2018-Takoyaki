5449c3d06d7508c8080618fec4452fec37295ce9
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2018-06-30 12:12:14 +0800

    fix

--- MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/PointsActivity.java
+++ MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/PointsActivity.java
@@ -30,60 +30,59 @@ public class PointsActivity extends AppCompatActivity {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
         final TextView pointsHelp = (TextView) findViewById(R.id.points_help);
-
-        FirebaseRecyclerAdapter<PointHistory, PointsViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<PointHistory, PointsViewHolder>(PointsActivity.class,
-                        R.layout.reward_item, RewardsViewHolder.class, databaseReference.child(Rewards.NAME)) {
-                    @Override
-                    protected void populateViewHolder(final PointsViewHolder viewHolder, final PointHistory model, int position) {
-
-                        //viewHolder.pt_points.setText();
-                        final String uid = model.getUid();
-
-                        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(Users.NAME);
-
-                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                for (DataSnapshot snap: dataSnapshot.getChildren()) {
-                                    Users u = snap.getValue(Users.class);
-                                    if (u.getUid().equals(uid)) {
-                                        viewHolder.pt_points.setText((int) u.getPoints());
-                                        return;
-                                    }
-                                }
-                            }
-
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-
-                            }
-                        });
-
-                        viewHolder.pt_name.setText(model.);
-
-
-                    }
-
-                    @Override
-                    protected void populateViewHolder(RewardsViewHolder viewHolder, Rewards model, int position) {
-                        viewHolder.rewardDesc.setText(model.getDesc());
-                        //TODO fix this
-                        viewHolder.rewardValidity.setText(model.getSponsor());
-
-                        String key = getRef(position).getKey ();
-                        viewHolder.itemView.setTag (key);
-
-                        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Intent i = new Intent(getBaseContext(), RewardInfoActivity.class);
-                                i.putExtra("key", view.getTag().toString());
-                                startActivity(i);
-                            }
-                        });
-                    }
-                };
+//
+//        FirebaseRecyclerAdapter<PointHistory, PointsViewHolder> firebaseRecyclerAdapter =
+//                new FirebaseRecyclerAdapter<PointHistory, PointsViewHolder>(PointsActivity.class,
+//                        R.layout.reward_item, RewardsViewHolder.class, databaseReference.child(Rewards.NAME)) {
+//                    @Override
+//                    protected void populateViewHolder(final PointsViewHolder viewHolder, final PointHistory model, int position) {
+//
+//                        //viewHolder.pt_points.setText();
+//                        final String uid = model.getUid();
+//
+//                        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(Users.NAME);
+//
+//                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+//                            @Override
+//                            public void onDataChange(DataSnapshot dataSnapshot) {
+//                                for (DataSnapshot snap: dataSnapshot.getChildren()) {
+//                                    Users u = snap.getValue(Users.class);
+//                                    if (u.getUid().equals(uid)) {
+//                                        viewHolder.pt_points.setText((int) u.getPoints());
+//                                        return;
+//                                    }
+//                                }
+//                            }
+//
+//                            @Override
+//                            public void onCancelled(DatabaseError databaseError) {
+//
+//                            }
+//                        });
+//
+////                        viewHolder.pt_name.setText(model.getQRID());
+//
+//
+//                    }
+//
+//                    protected void populateViewHolder(RewardsViewHolder viewHolder, Rewards model, int position) {
+//                        viewHolder.rewardDesc.setText(model.getDesc());
+//                        //TODO fix this
+//                        viewHolder.rewardValidity.setText(model.getSponsor());
+//
+//                        String key = getRef(position).getKey ();
+//                        viewHolder.itemView.setTag (key);
+//
+//                        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                Intent i = new Intent(getBaseContext(), RewardInfoActivity.class);
+//                                i.putExtra("key", view.getTag().toString());
+//                                startActivity(i);
+//                            }
+//                        });
+//                    }
+//                };
 
         pointsHelp.setOnClickListener(new View.OnClickListener() {
             @Override
