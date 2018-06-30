c169a87aa09c33e4087c60699e15142f700c8827
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2018-06-30 10:56:00 +0800

    commit

--- MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/MyRewardsActivity.java
+++ MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/MyRewardsActivity.java
@@ -1,8 +1,11 @@
 package ph.edu.dlsu.takoyaki.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -27,9 +30,26 @@ public class MyRewardsActivity extends AppCompatActivity {
                         R.layout.reward_item, RewardsViewHolder.class, databaseReference.child(Rewards.NAME)) {
                     @Override
                     protected void populateViewHolder(RewardsViewHolder viewHolder, Rewards model, int position) {
+                        viewHolder.rewardDesc.setText(model.getDesc());
+                        //TODO fix this
+                        viewHolder.rewardValidity.setText(model.getSponsor());
 
+                        String key = getRef(position).getKey ();
+                        viewHolder.itemView.setTag (key);
+
+                        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent i = new Intent(getBaseContext(), RewardInfoActivity.class);
+                                i.putExtra("key", view.getTag().toString());
+                                startActivity(i);
+                            }
+                        });
                     }
                 };
+
+        myRewardsRV.setAdapter(firebaseRecyclerAdapter);
+        myRewardsRV.setLayoutManager (new LinearLayoutManager(getBaseContext(), LinearLayoutManager.VERTICAL, false));
 
     }
 }
--- MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/PointsActivity.java
+++ MyApplication/app/src/main/java/ph/edu/dlsu/takoyaki/myapplication/PointsActivity.java
@@ -1,7 +1,10 @@
 package ph.edu.dlsu.takoyaki.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PointsActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class PointsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points);
+
+        final TextView pointsHelp = (TextView) findViewById(R.id.points_help);
+
+        pointsHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setClass(getBaseContext(), Tutorial.class);
+                startActivity(i);
+            }
+        });
     }
 }
